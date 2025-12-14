1ac85e21fc15d0af4f0f01e4fbbc28a009393fbd
Thamarai <tmanalan710@gmail.com>
2025-12-14 19:14:11 +0530

    Design inspired ny shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Nav from "./ui/home/nav-bar"
 import Footer from "./ui/home/footer"
 import Header from "./ui/home/header"
-import Section1 from "./ui/home/section-1"
 
 export default function Home(){
   return <>
     <Nav/>
     <Header/>
-    <Section1/>
 
   </>
 }
